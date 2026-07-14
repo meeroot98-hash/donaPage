4cab1f3eb5ff22494d132511a442dba8f90f920c
kitroot98 <meeroot98@gmail.com>
2026-07-14 16:58:36 -0500

    agregando enlaces

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,79 @@ const botonDonar = document.getElementById("botonDonar")
     cajaMenu.style.width= "100%";
     cajaMenu.style.height= "100%";
     cajaMenu.style.backgroundColor="rgb(14, 26, 12)"
-    document.body.appendChild(cajaMenu);
+    botonDonar.style.transition = "background-color 0.3s ease";
+
+
+    ////efefcto en boto Menu/////////////////
+    botonMenu.addEventListener("touchstart", ()=>{
+
+                botonMenu.style.backgroundColor="#ffffff"
+                botonMenu.style.color="#000000"
+    })
+    botonMenu.addEventListener("touchend",()=>{
+                botonMenu.style.backgroundColor="#1F618D";
+                botonMenu.style.color="#ffffff"
+
+    })
+    
+
+
+    document.body.appendChild(cajaMenu);/////////////////////creando menu despleglable
+
+    const cajaLista= document.createElement("div");
+    cajaLista.id = "cajaLista";
+    cajaLista.style.width="300px";
+    cajaLista.style.height="450px";
+    cajaLista.style.backgroundColor="#ff00f0";
+    cajaMenu.appendChild(cajaLista);//////////////////////////// creando Lista adentro
+    
+    const menuLista= document.createElement("li");
+    cajaLista.appendChild(menuLista);
+    menuLista.id="lista";
+
+
+    const inicio = document.createElement("a");
+    inicio.href="index.html";
+    inicio.innerText="Inicio"
+    menuLista.appendChild(inicio);
+
+
+    const paises = document.createElement("a");
+    paises.href="#";
+    paises.innerText="Paises";
+    menuLista.appendChild(paises);
+
+    const quienes = document.createElement("a");
+    quienes.href="#";
+    quienes.innerText="Quienes Somos";
+    menuLista.appendChild(quienes);
+
+
+    const agradecimiento = document.createElement("a");
+    agradecimiento.href="#";
+    agradecimiento.innerText="Agredecimiento";
+    menuLista.appendChild(agradecimiento);
+
+    
+
+
+
+    inicio.classList.add("conjunto")
+    paises.classList.add("conjunto");
+    quienes.classList.add("conjunto");
+    agradecimiento.classList.add("conjunto");    
+    
+    menuLista.appendChild(paises);
+    menuLista.appendChild(quienes);
+    menuLista.appendChild(agradecimiento);
+    
+
+    
+    
+
+
+
+
 
 
    
